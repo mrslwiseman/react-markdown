47f66fde44a2e93793f4cb75d79e2d2b815951db
Matt Wiseman <mrslwiseman@gmail.com>
2018-01-20 18:44:58 +1100

    add responsive feature

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,67 +1,105 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown'
 import fs from 'fs'
-import 'normalize.css'; 
+import 'normalize.css';
 import '../styles/App.scss';
 import axios from 'axios'
-
+import { debounce } from 'lodash'
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      md: ''
+      md: '',
+      showPreviewBar: true,
+      showEditor: true
     };
   }
 
-loadSampleData = () => {
-  
-axios.get('/api/sample')
-.then(res => this.setState({md: res.data}))
-.catch(e => alert('That didnt work.' + e))
+  loadSampleData = () => {
 
-}
+    axios.get('/api/sample')
+      .then(res => this.setState({ md: res.data }))
+      .catch(e => alert('That didnt work.' + e))
 
-  render() {
-    return (
-      <div className="app">
-        <header>
-          <h1>React Markdown Previewer</h1>
-          <nav>
+  }
+
+  toggleBar = () => {
+    // if both editor and preview are visible, toggle preview
+    const { showEditor, showPreviewBar } = this.state;
+
+    this.setState({
+      showPreviewBar: !showPreviewBar,
+      showEditor: !showEditor
+
+    })
+  }
+
+  setDimensionsState = () => {
+    const { showEditor, showPreviewBar } = this.state;
+    if (window.innerWidth < 768) {
+      if (showEditor && showPreviewBar) {
+        // ie its in full screen mode
+        this.setState({
+          showEditor: true,
+          showPreviewBar: false
+        })
+      }
+    } else {
+      this.setState({
+        showEditor: true,
+        showPreviewBar: true
+      })
+    }
+  }
+
+    componentWillMount(){
+      this.setDimensionsState()
+      window.addEventListener('resize', debounce(() => this.setDimensionsState(), 50))
+    }
+    componentWillUnmount(){
+      window.removeEventListener('resize')
+    }
+    render() {
+      return (
+        <div className="app">
+          <header>
+            <h1>React Markdown Previewer</h1>
+            <nav>
               <button className="nav__button" onClick={this.loadSampleData}>LOAD SAMPLE DATA</button>
-              <button className="nav__button">Share</button>
+              <button className="nav__button" onClick={this.toggleBar}>Toggle Preview</button>
               <button className="nav__button">Upload .md</button>
               <button className="nav__button">Download HTML</button>
-          </nav>
-        </header>
-        <main className="main">
-          <div className="input">
-            <textarea 
-              onChange = {e => this.setState({md: e.target.value})} 
-              ref={input => this.textarea = input} 
-              name="md-input" 
-              id="md-input" 
-              cols="30" 
-              rows="10"
-              spellcheck
-              autofocus
-              value={this.state.md}
-              >
-            </textarea>
-            </div>
-            <div className="output">
-            <ReactMarkdown className={'output__rendered'} source={this.state.md} />
-            </div>
-            </main>
-            <footer>
-              <button>Upload .md</button>
-              <button>Download .md</button>
-              <button>Copy to Clipboard</button>
-              <button>Download .html</button>
-              <button>Copy to Clipboard</button>
-            </footer>
-      </div>
-    );
-  }
-}
+            </nav>
+          </header>
+          <main className="main">
+            {
+              this.state.showEditor &&
+              <div className="input">
+                <textarea
+                  onChange={e => this.setState({ md: e.target.value })}
+                  ref={input => this.textarea = input}
+                  name="md-input"
+                  id="md-input"
 
-export default App;
+                  spellCheck={true}
+                  autoFocus={true}
+                  value={this.state.md}
+                >
+                </textarea>
+              </div>
+            }
+
+            {
+              this.state.showPreviewBar &&
+                <ReactMarkdown className={'output'} source={this.state.md} />
+
+
+            }
+          </main>
+          
+        </div>
+      );
+    }
+  }
+
+  export default App;
