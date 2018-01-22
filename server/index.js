d762ef3b7bc1dcc3518da16634ba27e9c091fec6
Matt Wiseman <mrslwiseman@gmail.com>
2018-01-22 20:14:36 +1100

    remove upload from express

--- server/index.js
+++ server/index.js
@@ -3,43 +3,13 @@ const app = express();
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require("fs"));
 const path = require('path');
-const Busboy = require('busboy');
-
-
-app.post('/api/upload', (req,res) => {
-    var busboy = new Busboy({ headers: req.headers });
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-      console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-      file.on('data', function(data) {
-        console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-        res.json(String(data))
-      });
-      file.on('end', function() {
-        console.log('File [' + fieldname + '] Finished');
-      });
-    });
-    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
-    });
-    busboy.on('finish', function() {
-      console.log('Done parsing form!');
-    });
-    req.pipe(busboy);
-})
 
 app.get('/api/sample', (req, res) => {
-    console.log('/api/sample');
-
     fs.readFileAsync("markdown.md")
         .then(data => data.toString())
         .then(data => res.json(data) )
         .catch(e => res.json('There was an error.'))
-
-
 })
-
-
-
 
 app.set('port', process.env.PORT || 5000);
 
