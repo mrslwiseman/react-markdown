8ff445a7b67461925d8f3df83f7c78c302a97927
Matt Wiseman <mrslwiseman@gmail.com>
2018-01-25 13:30:23 +1100

    edit header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,15 +2,15 @@ import React from 'react'
 import Icon from './Icon'
 import { ICONS } from '../constants'
 
-const Header = ({onToggleEditorBar, onTogglePreviewBar, loadSampleData, loadFile}) => {
+const Header = ({onToggleEditorBar, onTogglePreviewBar, onToggleSplitView, loadSampleData, loadFile, inputPaneVisible, outputPaneVisible}) => {
     const onFileUploadClick = () => {
         const fileInput = document.querySelector('input[type="file"]')
         fileInput.click();
     }
     return (
         <header>
-            <h1>Yame.</h1>
-            <p>Yet Another Markdown Editor.</p>
+            <h1 className='logo'>Yame.</h1>
+            <p className='tagline'>Yet Another Markdown Editor.</p>
             <nav>
                 <ul>
 
@@ -20,34 +20,22 @@ const Header = ({onToggleEditorBar, onTogglePreviewBar, loadSampleData, loadFile
                         <Icon icon={ICONS.UPLOAD} />
                         <input onChange={loadFile} type="file" />
                     </button>
-                    <button 
-                        className="nav__button" 
-                        onClick={onToggleEditorBar}>
-                        <Icon icon={ICONS.EDIT} />
-                        <span>Toggle Editor</span>
-                    </button>
-                    <button 
-                        className="nav__button" 
-                        onClick={onTogglePreviewBar}>
-                        <Icon icon={ICONS.PREVIEW} />
-                        <span>Toggle Preview</span>
-                    </button>
+                    
+
+                    
+                    
                     <button 
                         className="nav__button">
                         <Icon icon={ICONS.DOWNLOAD} />
                         <span>Download</span>
                     </button>
-                    <button 
-                        className="nav__button"> 
-                        <Icon icon={ICONS.SHARE} />
-                        <span>Share</span>
-                    </button>
+              
 
                     <button 
                         className="nav__button" 
                         onClick={loadSampleData}>
                         Load Sample
-              </button>
+                    </button>
                 </ul>
 
             </nav>
