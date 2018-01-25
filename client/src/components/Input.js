500352692469be1fc41729d3343d41f3e3ac93cf
Matt Wiseman <mrslwiseman@gmail.com>
2018-01-25 13:31:29 +1100

    add toolbar with conditional rendering

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -2,38 +2,64 @@ import React from 'react'
 import Icon from './Icon'
 import { ICONS } from '../constants'
 
-const Input = ({ onInputChange, source }) => {
+const Input = ({ onInputChange, source, onToggleEditorBar, onTogglePreviewBar, inputPaneVisible, outputPaneVisible, onToggleSplitView }) => {
     const onCopyInput = () => {
         window.getSelection().removeAllRanges();
         const input = document.querySelector('.input__src')
         const range = document.createRange();
         range.selectNode(input);
         window.getSelection().addRange(range);
-        
+
         try {
-          document.execCommand('copy');
+            document.execCommand('copy');
         } catch (err) {
-          console.log('Oops, unable to copy');
+            console.log('Oops, unable to copy');
         }
-      }
+    }
 
     return (
         <div className="input">
-        <div className="view__buttons">
-          <button onClick={onCopyInput}>
-            <Icon icon={ICONS.COPY} />
-          </button>
-        </div>
-        <textarea
-          onChange={(e) => onInputChange(e.target.value)}
-          className="input__src"
-          spellCheck={true}
-          autoFocus={true}
-          value={source}
-        >
-        </textarea>
+            <div className="view__buttons">
 
-      </div>
+
+                {
+                    !outputPaneVisible && inputPaneVisible &&
+                    <button
+                        onClick={onTogglePreviewBar}>
+                        <Icon icon={ICONS.PREVIEW} />
+                    </button>
+
+                }
+                {
+                    !outputPaneVisible && inputPaneVisible &&
+                    <button
+                        onClick={onToggleSplitView}>
+                        <Icon icon={ICONS.SPLIT} />
+                    </button>
+
+                }
+
+                {
+                    outputPaneVisible && inputPaneVisible &&
+                    <button onClick={onToggleEditorBar}>
+                        <Icon icon={ICONS.EXPAND} />
+                    </button>
+                }
+
+                <button onClick={onCopyInput}>
+                    <Icon icon={ICONS.COPY} />
+                </button>
+            </div>
+            <textarea
+                onChange={(e) => onInputChange(e.target.value)}
+                className="input__src"
+                spellCheck={true}
+                autoFocus={true}
+                value={source}
+            >
+            </textarea>
+
+        </div>
     )
 }
 
--- client/src/components/Output.js
+++ client/src/components/Output.js
@@ -4,7 +4,7 @@ import Icon from './Icon'
 import { ICONS } from '../constants'
 
 
-const Output = ({ source }) => {
+const Output = ({ source, onToggleEditorBar, onTogglePreviewBar, inputPaneVisible, outputPaneVisible, onToggleSplitView }) => {
     const onCopyOutput = (e) => {
         const input = document.createElement('input')
         const outputHTML = document.querySelector('.output__src').innerHTML
@@ -13,16 +13,48 @@ const Output = ({ source }) => {
         input.select()
         document.execCommand('copy')
         document.body.removeChild(input)
-      }
+    }
     return (
         <div className="output">
-              <div className="view__buttons">
-                <button onClick={(e) => onCopyOutput(e)}>
-                  <Icon icon={ICONS.COPY} />
+            <div className="view__buttons">
+                {
+                    outputPaneVisible && !inputPaneVisible &&
+                    <button
+                        onClick={onToggleEditorBar}>
+                        <Icon icon={ICONS.EDIT} />
+                    </button>
+
+
+                }
+                {
+                    outputPaneVisible && !inputPaneVisible &&
+                    <button
+                    onClick={onToggleSplitView}>
+                    <Icon icon={ICONS.SPLIT} />
                 </button>
-              </div>
-              <ReactMarkdown className="output__src" source={source}></ReactMarkdown>
+
+
+                }
+                {
+                    !outputPaneVisible && inputPaneVisible &&
+                    <button
+                        onClick={onTogglePreviewBar}>
+                        <Icon icon={ICONS.PREVIEW} />
+                    </button>
+                }
+                {
+                    outputPaneVisible && inputPaneVisible &&
+                    <button onClick={onTogglePreviewBar}>
+                        <Icon icon={ICONS.EXPAND} />
+                    </button>
+                }
+                <button onClick={(e) => onCopyOutput(e)}>
+                    <Icon icon={ICONS.COPY} />
+                </button>
+
             </div>
+            <ReactMarkdown className="output__src" source={source}></ReactMarkdown>
+        </div >
     )
 }
 
