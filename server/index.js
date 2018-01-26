3a396d96e82a3bb4fad8b3c6f77b5ad2eb44c6f6
Matt Wiseman <mrslwiseman@gmail.com>
2018-01-26 22:42:06 +1100

    fix server

--- server/index.js
+++ server/index.js
@@ -4,13 +4,21 @@ const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require("fs"));
 const path = require('path');
 
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get('/api/sample', (req, res) => {
-    fs.readFileAsync("markdown.md")
+    fs.readFileAsync(path.resolve(__dirname, '../client/build', 'markdown.md'))
         .then(data => data.toString())
         .then(data => res.json(data) )
-        .catch(e => res.json('There was an error.'))
+        .catch(e => console.log('Error sending sample data.'))
 })
 
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+  
 app.set('port', process.env.PORT || 5000);
 
 const server = app.listen(app.get('port'), () => {
